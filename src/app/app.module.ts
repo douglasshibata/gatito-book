6840f5158a1fabe2a180b83d5c02b8673e7748e9
Douglas Shibata <douglas.shibata11@gmail.com>
2021-08-18 13:20:02 -0300

    adicionando cabecalho e rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    CabecalhoModule,
+    RodapeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
